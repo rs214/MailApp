6187dcfbdea63259fd752579c0c2bd9d91c589c1
rs214 <robertsi0214@gmail.com>
2018-01-30 11:42:02 -0800

    implement basic google stretegy for authentication

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
-const passport = require('passsport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({bye:'julie cutie'});
-});
+app.get('/', (req, res) =>{
+  res.send({hello: 'julie'})
+})
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+  (accessToken, profile, refreshToken, done) => {
+    console.log(accessToken);
+  })
+);
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

7444e74980bc310593364832b09112a1a4cada41
rs214 <robertsi0214@gmail.com>
2018-01-30 11:16:15 -0800

    add basic google oauth setup

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const passport = require('passsport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({bye:'julie cutie'});
 });
 
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

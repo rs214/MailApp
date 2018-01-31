73cd86bae33f1d54bbf823f307042fb78058c1f2
rs214 <robertsi0214@gmail.com>
2018-01-31 10:54:54 -0800

    add switch statements to show UI depending on auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Header extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Log In with Google</a></li>
+        )
+      default:
+        return (
+          <li><a>Logout</a></li>
+        )
+    }
+  }
+
+
   render() {
     return (
       <nav>
@@ -9,9 +27,7 @@ class Header extends Component {
             MailApp
           </a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +35,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);

033ace003f3f190b7620d1a87f6a564c06771742
rs214 <robertsi0214@gmail.com>
2018-02-05 13:05:51 -0800

    complete sendgrid functionality on server side

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,10 +6,48 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sendGridAPI = sendgrid(keys.sendGridkey);
     this.from_email = new helper.Email('no-reply@mailapp.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackignSettings = new helper.TrackingSettings();
+    const clickTracking = new hlper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sendGridAPI.emptyRequest({
+      method: POST,
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sendGridAPI.API(request)
+    return response;
   }
 }
 

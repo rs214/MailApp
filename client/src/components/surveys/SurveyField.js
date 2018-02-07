f9157e9641ffe36d590604f1138ba25150979b38
rs214 <robertsi0214@gmail.com>
2018-02-06 16:41:08 -0800

    render surveyForms using reduxForm

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+//contains logic to render single label and text input
+import React from 'react';
+
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input}/>
+    </div>
+  )
+}
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,16 @@
+//container for surveyform and surveyreview
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        SurveyNew
+        <SurveyForm />
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;

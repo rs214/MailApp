b32947a783b3fd21402d03c4ee5d25a0bca6ae83
rs214 <robertsi0214@gmail.com>
2018-02-07 16:59:51 -0800

    hooked up withRouter for redirect back to dashboard

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,6 +1,6 @@
 //container for surveyform and surveyreview
 import React, { Component } from 'react';
-import { reduxForm } from 'reduxForm';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
